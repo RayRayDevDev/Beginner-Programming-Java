aeef9cb9e5c8059a90142a8e1bac9a9934bdac38
RäDev <github@rayraydev.dev>
2022-05-20 18:37:52 -0600

    Multiple changes.

--- ProjectTwoOOPClass.java
+++ ProjectTwoOOPClass.java
@@ -4,23 +4,30 @@ import static java.lang.System.out;
 //Created by Cole Stanley (RäDev) for COP 3330C
 
 public abstract class Loan implements LoanConstants {
-    protected int loanNum;
-    protected String lastName;
-    protected double loanAmt;
-    protected double interestRate;
-    protected int loanTerm;
-    protected double currentPrimeRate;
+    protected int loanNum;  //Loan Number
+    protected String lastName;  //Customer's Last Name
+    protected double loanAmt;  //Requested Loan Amount
+    protected double currentPrimeInterestRate;  //Prime Interest Rate in percentage form
+    protected double personalInterestRate;  //Personal Loan Combined Interest Rate in decimal form
+    protected double businessInterestRate;  //Business Loan Combined Interest Rate in decimal form
+    protected int loanTerm;  //Requested Loan Term
 
-    protected Loan(int lnNum, String LstNm, double lnmt, int lnTerm, double intRate, double currPrmRt) { //"Ln" = "Loan."
+    protected Loan(int lnNum, String LstNm, double lnmt, int lnTerm) { //"Ln" = "Loan."
         if(lnmt > maxLoanAmt) out.println("Loans cannot be made above $50,000. Please try again with an amount less than or equal to this amount. Thank you."); break;
         if(lnTerm != shortTerm || lnTerm != medTerm || lnTerm != longTerm) lnTerm = shortTerm;
         lnNum = loanNum;
         LstNm = lastName;
         lnmt = loanAmt;
         lnTerm = loanTerm;
-        intRate = interestRate;
-        currPrmRt = currentPrimeRate;
 }
+    public static double percentToDecimalCalc(double userInputtedPercentage) {
+        double finalDecimalValue = userInputtedPercentage / 100;
+        return finalDecimalValue;
+    }
+    public String toString() {
+        String loanInfo = "Loan Number: " + loanNum + ".\n" + "Last Name: " + lastName + ".\n" + "Requested Loan Amount: " + loanAmt + ".\n" + "Interest Rate: " + currentPrimeInterestRate + ".\n" + "Loan Term: " + loanTerm + ".\n";
+        return loanInfo;
+    }
 
 }
 interface LoanConstants {
@@ -32,10 +39,13 @@ interface LoanConstants {
 }
 class BusinessLoan extends Loan {
 
-    protected BusinessLoan(int lnNum, String LstNm, double lnmt, int lnTerm, double intRate, double currPrmRt) {
-        super(lnNum, LstNm, lnmt, lnTerm, intRate, currPrmRt);
-        interestRate = 0.01 + currentPrimeRate;
+    protected BusinessLoan(int lnNum, String LstNm, double lnmt, int lnTerm, double prmRte) {
+        super(lnNum, LstNm, lnmt, lnTerm);
+        currentPrimeInterestRate = Loan.percentToDecimalCalc(prmRte);
+    }
 
+    public double businessInterestRate(double currentPrimeInterestRate) {
+        return businessInterestRate = 0.01 + currentPrimeInterestRate;
     }
 
 
@@ -43,10 +53,13 @@ class BusinessLoan extends Loan {
 }
 class PersonalLoan extends Loan {
 
-    protected PersonalLoan(int lnNum, String LstNm, double lnmt, int lnTerm, double intRate, double currPrmRt) {
-        super(lnNum, LstNm, lnmt, lnTerm, intRate, currPrmRt);
-        interestRate = 0.02 + currentPrimeRate;
+    protected PersonalLoan(int lnNum, String LstNm, double lnmt, int lnTerm, double prmRte) {
+        super(lnNum, LstNm, lnmt, lnTerm);
+        currentPrimeInterestRate = Loan.percentToDecimalCalc(prmRte);
+    }
 
+    public double personalInterestRate(double currentPrimeInterestRate) {
+        return personalInterestRate = 0.02 + currentPrimeInterestRate;
     }
 
 }
