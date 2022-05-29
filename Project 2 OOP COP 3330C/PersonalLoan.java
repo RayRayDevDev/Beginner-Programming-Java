629eebffea7f7c4730886488c4e3a71e06af0e78
RäDev <github@rayraydev.dev>
2022-05-29 17:16:56 -0600

    Changing from protected to public in Personal Loan.

--- Project 2 OOP COP 3330C/PersonalLoan.java
+++ Project 2 OOP COP 3330C/PersonalLoan.java
@@ -2,7 +2,7 @@ import static java.lang.System.out;
 
 public class PersonalLoan extends Loan {
     static double personalTotal;
-    protected PersonalLoan(int lnNum, String LstNm, double lnmt, int lnTerm, double prmRte) {
+    public PersonalLoan(int lnNum, String LstNm, double lnmt, int lnTerm, double prmRte) {
         super(lnNum, LstNm, lnmt, lnTerm);
         double finalDecimalValue = percentToDecimalCalc(prmRte);
         personalInterestRate = finalDecimalValue + 0.02;
