e7ea993bbdbb0cc9f22175412c01998cd4918386
RäDev <github@rayraydev.dev>
2022-05-28 20:33:48 -0600

    Removed debugging output from pcompleted parts of the project.

--- Project 2 OOP COP 3330C/BusinessLoan.java
+++ Project 2 OOP COP 3330C/BusinessLoan.java
@@ -7,11 +7,8 @@ public class BusinessLoan extends Loan {
     protected BusinessLoan(int lnNum, String LstNm, double lnmt, int lnTerm, double prmRte) {
         super(lnNum, LstNm, lnmt, lnTerm);
         double finalDecimalValue = Loan.percentToDecimalCalc(prmRte);
-        out.println(finalDecimalValue);
         businessInterestRate = finalDecimalValue + 0.01;
-        out.println(businessInterestRate);
         double businessTotal = businessLoanMath(lnmt);
-        out.println(businessTotal);
     }
 
 
