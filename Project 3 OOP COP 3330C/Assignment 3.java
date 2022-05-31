ff94456d7b901debc4f8fcb971f8648b132d71fb
RäDev <github@rayraydev.dev>
2022-05-30 23:29:22 -0600

    Fixed first exception issues.

--- Project 3 OOP COP 3330C/Assignment 3.java
+++ Project 3 OOP COP 3330C/Assignment 3.java
@@ -1,34 +1,65 @@
-import java.lang.System.out;
+import static java.lang.System.out;
+
+import java.util.Scanner;
 
 class RadioStationException extends Exception {
-    String radioCallSign;
-    double radioStationFrequency;
 
-    public RadioStationException(String radioCallSign, double radioStationFrequency) {
-        super(radioCallSign, radioStationFrequency);
-        if(radioCallSign.length() != 4) {
-            throw RadioStationException("Radio Stations cannot have a callsign with length: " + out.print(radioCallSign.length()) + ". " + "Please enter a callsign such that it contains at least four (4) letters. Thank you!");
-        } 
-        else if(radioStationFrequency < 88 || radioStationFrequency > 108) {
-            throw RadioStationException("Radio Statons must have a carrier frequency between 88 and 108. You entered: " + out.print(radioStationFrequency) + ". " + "Please ensure the carrier frequency is between 88 and 108 and try again. Thank you!");
-        }
-    radioCallSign = this.radioCallSign;
-    radioStationFrequency = this.radioStationFrequency;
+
+    public RadioStationException(String message) {
+        super(message);
     }
+
 }
 
-class RadioStation extends RadioStationException {
-    String radioCallSign;
-    double radioStationFrequency;
+class RadioStation {
+     private static String radioCallSign;
+     private static double radioStationFrequency;
+     private static int i;
+     private static int j;
+    public RadioStation(String rCS, double rSF) {
+        radioCallSign = rCS;
+        radioStationFrequency = rSF;
+    }
 
-    public RadioStation(String radioCallSign, double radioStationFrequency) {
-        super(radioCallSign, radioStationFrequency);
+    static void validate (String radioCallsign) throws RadioStationException {
+        for(int j = 0; j < radioCallSign.length(); j++){
+            if(Character.isDigit(radioCallSign.charAt(j))) {
 
-        radioCallSign = this.radioCallSign;
-        radioStationFrequency = this.radioStationFrequency;
+                throw new RadioStationException("Station call signs must only consist of letters. Please try again.");
+            }
+
+            else{
+                
+                continue;
+            }
+        }
     }
 
     public static void main(String[] args) {    
-        
+        RadioStation[] stationArray = new RadioStation[6];
+        Scanner userInput = new Scanner(System.in);
+        for(i = 0; i != 7; i++) {
+            try {
+                out.print("Welcome! Please enter the callsign of the radio station you'd like to listen to. Please enter only letters and ensure that the callsign is no more than four (4) letters in length:  ");
+                radioCallSign = userInput.nextLine(); 
+                validate(radioCallSign);
+                out.print("Next, please enter the carrier frequency of the desired radio station. Please note that the frequency cannot be below 88 or above 108. ");
+                radioStationFrequency = userInput.nextDouble();
+                stationArray[i] = new RadioStation(radioCallSign, radioStationFrequency); 
+            } catch(RadioStationException e) {
+                out.println(e.getMessage());;
+            }
+                
+
+            }
     }
 }
+
+
+
+// if(rCS.length() != 4 || RadioStation.isANumber == true) {
+//     throw new RadioStationException("Radio Stations cannot have a callsign with length: " + out.print(Integer.toString(rCS.length())) + ". " + "Please enter a callsign such that it contains at least four (4) letters exactly. Thank you!");
+// } 
+// else if(rSF < 88 || rSF > 108) {
+//     throw new RadioStationException("Radio Statons must have a carrier frequency between 88 and 108. You entered: " + out.print(rSF) + ". " + "Please ensure the carrier frequency is between 88 and 108 and try again. Thank you!");
+// }
