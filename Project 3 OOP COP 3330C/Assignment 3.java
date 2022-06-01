839db3281878242be6cf7ca2b720190a0f528b9f
RäDev <github@rayraydev.dev>
2022-05-31 22:09:06 -0600

    Many, many changes. Added a try catch method. Added exception methods for station numbers.

--- Project 3 OOP COP 3330C/Assignment 3.java
+++ Project 3 OOP COP 3330C/Assignment 3.java
@@ -12,47 +12,65 @@ class RadioStationException extends Exception {
 }
 
 class RadioStation {
-     private static String radioCallSign;
-     private static double radioStationFrequency;
-     private static int i;
-     private static int j;
+     private static String radioCallSign = null;
+     private static double radioStationFrequency = 0;
+     private static int i = 0;
     public RadioStation(String rCS, double rSF) {
         radioCallSign = rCS;
         radioStationFrequency = rSF;
     }
 
-    static void validate (String radioCallsign) throws RadioStationException {
+    static void validateCallSign (String radioCallsign) throws RadioStationException {
         for(int j = 0; j < radioCallSign.length(); j++){
-            if(Character.isDigit(radioCallSign.charAt(j))) {
+            if(Character.isDigit(radioCallSign.charAt(j))) { //Check if there are any numbers that were entered.
 
-                throw new RadioStationException("EXCEPTION: Station call signs must only consist of letters. Please try again.");
+                throw new RadioStationException("EXCEPTION in MAIN: Station call signs must only consist of letters. Unable to complete radio station object!");
             }
+        }
+        int radioCallSignLength = radioCallsign.length();
 
-            else{
+        if(radioCallSignLength < 4 || radioCallSignLength > 4) {
                 
-                continue;
-            }
+            throw new RadioStationException(" EXCEPTION in MAIN: Station call signs must contain exactly four (4) letters. Detected more or less than four (4) letters. Unable to create radio station object!");
+        }
+        
+        
+    }
+
+    static void validateStationFrequency (double radioStationFrequency) throws RadioStationException {
+        if(radioStationFrequency < 88.0 || radioStationFrequency > 108.0) {
+            throw new RadioStationException("EXCEPTION in MAIN: The station frequency must be within 88.0 and 108.0, inclusive. The inputted frequency is not within this range. Unable to create the radio station object!");
         }
     }
 
     public static void main(String[] args) {    
         RadioStation[] stationArray = new RadioStation[6];
         Scanner userInput = new Scanner(System.in);
-        for(i = 0; i != 7; i++) {
+        boolean exception = false;
+        do {
             try {
                 out.print("Welcome! Please enter the callsign of the radio station you'd like to listen to. Please enter only letters and ensure that the callsign is no more than four (4) letters in length:  ");
-                radioCallSign = userInput.nextLine(); 
-                validate(radioCallSign);
+                radioCallSign = userInput.next(); 
+                validateCallSign(radioCallSign);
                 out.print("Next, please enter the carrier frequency of the desired radio station. Please note that the frequency cannot be below 88 or above 108. ");
-                radioStationFrequency = userInput.nextDouble();
+                try {
+                    radioStationFrequency = userInput.nextDouble();
+                } catch (Exception e) {
+                    out.println("You did not enter a number. Please enter a number and try again.");
+                    exception = true;
+                }
+                if(exception){
+                    continue;
+                }
+                validateStationFrequency(radioStationFrequency);
                 stationArray[i] = new RadioStation(radioCallSign, radioStationFrequency); 
+                i++;
             } catch(RadioStationException e) {
                 out.println(e.getMessage());
-                System.exit(1);
             }
                 
 
-            }
+            } while(i < stationArray.length);
     }
 }
 
