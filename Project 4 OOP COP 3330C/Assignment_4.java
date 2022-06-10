7283638cf222581ac8fad629be30d0a95427bd14
RäDev <github@rayraydev.dev>
2022-06-09 22:05:31 -0600

    Multiple changes.

--- Project 4 OOP COP 3330C/Assignment_4.java
+++ Project 4 OOP COP 3330C/Assignment_4.java
@@ -8,7 +8,7 @@ class Animal implements Runnable {
     private float position = 0;  //The animal's position.
     private float speed = 0;  //The animal's speed.
     private int restMax = 0;  //The maxium amount (in ms) the animal is ever allowed to rest.
-    private long randomRest = Math.random(restMax);  //Determine's the random amount of time (up to the restMax) the animal may rest each time called.
+    private long randomRest;  //Determine's the random amount of time (up to the restMax) the animal may rest each time called.
     private boolean winner = false;  //Initial condition; nobody has raced, therefore nobody has won yet. 
 
     public void setName(String name) {
@@ -75,28 +75,38 @@ class Main {
     float animalStartPos;
     float animalStartSpeed;
     int animalRestMax;
+    int i;
 
+        Animal newAnimal = new Animal();
+        Animal[] animalArray =  new Animal[2];
+
+        for(i = 0; i < animalArray.length; i++) {
     out.print("\nHowdy! Welcome to the amazing race between two animals! Please enter the type of animal you want to race: ");
         animalName =  userInput.next();
+        newAnimal.setName(animalName);
+
         out.print("Please enter the animal's starting position in the format: xx.xx where 0 is the beginning of the race and 100 is the end: ");
         animalStartPos = userInput.nextFloat ();
             if(animalStartPos < 0 || animalStartPos > 100) {
              out.println("The Animal's starting position CANNOT be less than 0 or more than 100! Please try again!"); 
                 System.exit(1);
         }
+        newAnimal.setPosition(animalStartPos);
+
         out.print("Great! Now, please enter the Animal's speed in the format of xx.xx. Values can be from .25 to 10: ");
         animalStartSpeed = userInput.nextFloat();
         if(animalStartSpeed < 0.25 || animalStartSpeed > 10) {
             out.println("The Animal's speed CANNOT be less than 0.25 or more than 10! Please try again!");
             System.exit(2);
         }
-        out.print("Please enter the number of seconds the animal should rest between sprints using ONLY whole numbers: ");
-        animalRestMax = userInput.nextInt() / 1000;
+        newAnimal.setSpeed(animalStartSpeed);
 
-        Animal rabbit = new Animal(animalName, animalStartPos, animalStartSpeed, animalRestMax);
-        Animal.setName(animalName); 
+        out.print("Please enter the number of milliseconds the animal should rest between sprints using ONLY whole numbers: ");
+        animalRestMax = userInput.nextInt();
+        newAnimal.setRestMax(animalRestMax);
 
-        Animal turtle = new Animal(animalName, animalStartPos, animalStartSpeed, animalRestMax);
-
+        animalArray[i] = new Animal(newAnimal.getName(), newAnimal.getPosition(), newAnimal.getSpeed(), newAnimal.getRestMax());
+        
+        }
     }
 }
