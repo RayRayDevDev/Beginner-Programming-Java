4b0c02a4c26464865db5c552102246f8a2b24ce8
RäDev <github@rayraydev.dev>
2022-06-10 23:25:12 -0600

    Eat method working.

--- Project 4 OOP COP 3330C/Assignment_4.java
+++ Project 4 OOP COP 3330C/Assignment_4.java
@@ -1,6 +1,7 @@
 import static java.lang.System.out;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
 
 class Animal implements Runnable {
     private String name = null;  //The animal's name and also the thread's name.
@@ -9,13 +10,15 @@ class Animal implements Runnable {
     private int restMax = 0;  //The maxium amount (in ms) the animal is ever allowed to rest.
     private boolean winner = false;  //Initial condition; nobody has raced, therefore nobody has won yet. 
     private int randomRest = 0;
+    private Food animalFood;
     Scanner userInput = new Scanner(System.in);
 
-    Animal(String animalName, float animalStartPos, float animalStartSpeed, int animalRestMax) {
+    Animal(String animalName, float animalStartPos, float animalStartSpeed, int animalRestMax, Food animalFood) {
         name = animalName;
         position = animalStartPos;
         speed = animalStartSpeed;
         restMax = animalRestMax;
+        this.animalFood = animalFood;
         out.println(name + " has been created successfully!");
     }
     
@@ -34,7 +37,8 @@ class Animal implements Runnable {
                 randomRest = random.nextInt(restMax) + 1;
                 out.println("\nThe current animal, " + name + ", is at position: " + position + " and is moving at a speed of " + speed + " and is about to rest for " + randomRest + " milliseconds!\n");
                 Thread.sleep(randomRest);
-                
+                animalFood.eat(name, restMax);
+
                 if(position >= 100) {
                     winner = true;
                     
@@ -53,11 +57,31 @@ class Animal implements Runnable {
     }
 }
 
+class Food {
+    public synchronized void eat(String name, int restMax ) {
+        Random random = new Random();
+        int randomEat = random.nextInt(restMax);
+        try { 
+        if(name == "rabbit") {
+            randomEat = randomEat * 10;
+            out.println(name + " is currently eating and will be doing so for " + randomEat + " milliseconds!");
+            Thread.sleep(randomEat);
+        } else {
+        out.println(name + " is currently eating and will be doing so for " + randomEat + " milliseconds!");
+        Thread.sleep(randomEat);
+        }
+    } catch (InterruptedException e) {
+        out.println(e.getMessage());
+    }
+    }
+}
+
 class Main {
 
     public static void main(String[] args) {
-        Animal firstAnimal = new Animal("rabbit", 0, 5, 150);
-        Animal secondAnimal = new Animal("turtle", 0, 3, 100);
+        Food animalFood = new Food();
+        Animal firstAnimal = new Animal("rabbit", 0, 5, 150, animalFood);
+        Animal secondAnimal = new Animal("turtle", 0, 3, 100, animalFood);
         Thread first = new Thread(firstAnimal);
         Thread second = new Thread(secondAnimal);
         first.setName("firstAnimal");
