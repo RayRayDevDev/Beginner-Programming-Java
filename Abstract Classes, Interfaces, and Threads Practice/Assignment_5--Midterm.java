1a884503ce7d5dc1b27ec4bd1e85d51936a426e5
RäDev <github@rayraydev.dev>
2022-06-12 00:47:08 -0600

    Initial Commit.

---
+++ Abstract Classes, Interfaces, and Threads Practice/Assignment_5--Midterm.java
@@ -0,0 +1,22 @@
+import static java.lang.System.out;
+
+public abstract class Vehicle implements Runnable, MustImplement {
+
+} 
+
+
+
+
+public interface MustImplement {
+
+}
+
+
+
+class WriteResultsToFile {
+
+}
+
+class Main {
+    
+}
