c786a9d1ac024ffc8072852a491c84d3cb23fde1
RäDev <github@rayraydev.dev>
2022-06-15 22:00:11 -0600

    Initial BuildOut per assignment requirements.

--- Abstract Classes, Interfaces, and Threads Practice/Assignment_5--Midterm.java
+++ Abstract Classes, Interfaces, and Threads Practice/Assignment_5--Midterm.java
@@ -1,14 +1,107 @@
 import static java.lang.System.out;
 
 public abstract class Vehicle implements Runnable, MustImplement {
+protected String vehicleName;
+protected Double position;
+protected Double accelerationTime;
+protected Double Avgspeed;
+protected Double topSpeed;
+protected Double fuelOrElectricityCapacity;
+protected int refuelTime;
+
+Vehicle(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed, Double capacity, int refuelTime) {
+
+    vehicleName = name;
+    this.position = position;
+    accelerationTime = acceleration;
+    this.Avgspeed = avgSpeed;
+    this.topSpeed = topSpeed;
+    fuelOrElectricityCapacity = capacity;
+    this.refuelTime = refuelTime;
+
+}
+
 
 } 
+
+public class ToyotaCorollaCross extends Vehicle {
+
+    ToyotaCorollaCross(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed,
+            Double capacity, int refuelTime) {
+        super(name, position, acceleration, avgSpeed, topSpeed, capacity, refuelTime);
+        //TODO Auto-generated constructor stub
+    }
+    //2021 Toyota Corolla Cross--One of slowest in 2021.
+}
+
+public class VolkswagenTiguan extends Vehicle {
+
+    VolkswagenTiguan(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed,
+            Double capacity, int refuelTime) {
+        super(name, position, acceleration, avgSpeed, topSpeed, capacity, refuelTime);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
+public class NissanSentra extends Vehicle {
+
+    NissanSentra(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed, Double capacity,
+            int refuelTime) {
+        super(name, position, acceleration, avgSpeed, topSpeed, capacity, refuelTime);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
+public class BMWM8CompetitionGranCoupe extends Vehicle {
+
+    BMWM8CompetitionGranCoupe(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed,
+            Double capacity, int refuelTime) {
+        super(name, position, acceleration, avgSpeed, topSpeed, capacity, refuelTime);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
+public class LamborghiniHuracanSTO extends Vehicle {
+
+    LamborghiniHuracanSTO(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed,
+            Double capacity, int refuelTime) {
+        super(name, position, acceleration, avgSpeed, topSpeed, capacity, refuelTime);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
+public class Porsche911TurboSCabriolet extends Vehicle {
+
+    Porsche911TurboSCabriolet(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed,
+            Double capacity, int refuelTime) {
+        super(name, position, acceleration, avgSpeed, topSpeed, capacity, refuelTime);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
+public class TeslaModelSPlaid extends Vehicle {
+
+    TeslaModelSPlaid(String name, Double position, Double acceleration, Double avgSpeed, Double topSpeed,
+            Double capacity, int refuelTime) {
+        super(name, position, acceleration, avgSpeed, topSpeed, capacity, refuelTime);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
 
 
 
 
 public interface MustImplement {
-
+    public void accelerate();
+    public void drive();
+    public void refuelOrRecharge();
 }
 
 
