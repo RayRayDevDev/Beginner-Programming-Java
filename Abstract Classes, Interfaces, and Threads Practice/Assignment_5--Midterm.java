74d6d3af3502bf259e939b1a448138b834306711
RäDev <github@rayraydev.dev>
2022-06-19 01:41:07 -0600

    Unneded Import deletion.

--- Abstract Classes, Interfaces, and Threads Practice/Assignment_5--Midterm.java
+++ Abstract Classes, Interfaces, and Threads Practice/Assignment_5--Midterm.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 //Created by Cole Stanley (RäDev) for COP 3330C
