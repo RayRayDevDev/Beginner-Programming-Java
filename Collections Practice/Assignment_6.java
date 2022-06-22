eb592156a34d9c962073dbe76285847311693606
RäDev <github@rayraydev.dev>
2022-06-22 15:26:30 -0400

    Initial commit.

---
+++ Collections Practice/Assignment_6.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+
+import java.util.ArrayList;
+
+class Main {
+
+public static void main(String[] args) {
+    ArrayList<String> names = new ArrayList<String>();
+    out.println(names);
+}
+
+}
