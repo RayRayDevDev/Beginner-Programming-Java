afba66ce86192811b3b249d54b57335a89063f38
RäDev <github@rayraydev.dev>
2022-07-05 16:04:21 -0600

    Added ingredients class.

---
+++ JavaFX Practice/assignment_8/src/main/java/com/raydev/Sandwich.java
@@ -0,0 +1,22 @@
+package com.raydev;
+
+import java.util.ArrayList;
+
+public class Sandwich {
+    public static void ingredients() {
+        ArrayList<String> ingredients = new ArrayList<>();
+        ingredients.add("Salami");
+        ingredients.add("Chicken");
+        ingredients.add("Turkey");
+        ingredients.add("Roast Beef");
+        ingredients.add("Egg Salad");
+
+        ArrayList<String> bread = new ArrayList<>();
+        bread.add("White");
+        bread.add("Wheat");
+        bread.add("Rye");
+        bread.add("Ciabatta");
+        bread.add("Sourdough");
+    }
+
+}
