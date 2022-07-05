8c13ed278339ca6a0e46f9dfb292ce3df8ff5fed
RäDev <github@rayraydev.dev>
2022-07-05 16:05:36 -0600

    added finality to ArrayLists.

--- JavaFX Practice/assignment_8/src/main/java/com/raydev/Sandwich.java
+++ JavaFX Practice/assignment_8/src/main/java/com/raydev/Sandwich.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class Sandwich {
     public static void ingredients() {
-        ArrayList<String> ingredients = new ArrayList<>();
+        final ArrayList<String> ingredients = new ArrayList<>();
         ingredients.add("Salami");
         ingredients.add("Chicken");
         ingredients.add("Turkey");
         ingredients.add("Roast Beef");
         ingredients.add("Egg Salad");
 
-        ArrayList<String> bread = new ArrayList<>();
+        final ArrayList<String> bread = new ArrayList<>();
         bread.add("White");
         bread.add("Wheat");
         bread.add("Rye");
