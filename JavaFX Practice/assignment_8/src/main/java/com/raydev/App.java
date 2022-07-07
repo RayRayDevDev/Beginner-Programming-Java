14f0475230375d135557565850765be0c82ee048
RäDev <github@rayraydev.dev>
2022-07-06 23:07:57 -0600

    Changes.

--- JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
+++ JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,18 +20,10 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Valencia Sandwich Shop");
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        VBox vBox = new VBox(new Label("A JavaFX Label"));
+        scene = new Scene(vBox, 640, 480);
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
