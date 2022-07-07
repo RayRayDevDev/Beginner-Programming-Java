136410bf2d40b3c58fb16f097305a8a833701681
RäDev <github@rayraydev.dev>
2022-07-07 01:49:47 -0600

    Finally got the scene set properly.

--- JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
+++ JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
@@ -1,11 +1,13 @@
 package com.raydev;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 /**
@@ -13,13 +15,25 @@ import java.io.IOException;
  */
 public class App extends Application {
 
+    Label welcomeMessage = new Label("Welcome to Valencia's Sandwich Shop!");
+    Label breadMessage = new Label("Please select a type of Bread: ");
+    Label ingredientsMessage = new Label("Please select at least one filling: ");
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Valencia Sandwich Shop");
-        VBox vBox = new VBox(new Label("A JavaFX Label"));
-        scene = new Scene(vBox, 640, 480);
+        StackPane rootPane = new StackPane();
+        scene = new Scene(rootPane, 640, 480);
+        VBox vBox = new VBox(welcomeMessage);
+        vBox.setAlignment(Pos.TOP_CENTER);
+        VBox vBox2 = new VBox(breadMessage);
+        vBox2.setAlignment(Pos.CENTER_LEFT);
+        VBox vBox3 = new VBox(ingredientsMessage);
+        vBox3.setAlignment(Pos.CENTER_RIGHT);
+        rootPane.getChildren().addAll(vBox, vBox2, vBox3);
+        
+
         stage.setScene(scene);
         stage.show();
     }
