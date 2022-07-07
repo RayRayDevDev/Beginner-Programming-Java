f86fd90fb32d872aa42a8d4f2bd14b9d8df6d29f
RäDev <github@rayraydev.dev>
2022-07-06 23:09:59 -0600

    Deleting FXML.

--- JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
+++ JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
@@ -1,8 +1,6 @@
 package com.raydev;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
