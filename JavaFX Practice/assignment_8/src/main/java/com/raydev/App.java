63a228778dc68e2256893d61d1edc7595489abd7
RäDev <github@rayraydev.dev>
2022-07-07 01:51:00 -0600

    Deleted importy statement.

--- JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
+++ JavaFX Practice/assignment_8/src/main/java/com/raydev/App.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
