1e31c6be7b7cec2bf44ea83928b5facc319beedd
RäDev <github@rayraydev.dev>
2022-07-10 13:19:19 -0600

    Final initial commit.

--- JavaFX Practice/Assignment_8/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.raydev.assignment_8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.raydev.assignment_8 to javafx.fxml;
-    exports com.raydev.assignment_8;
-}
