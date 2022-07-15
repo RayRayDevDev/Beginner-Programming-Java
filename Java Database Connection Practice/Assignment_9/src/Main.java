8aaf2bf2709ecf038e00811ea7bdf0e72252702f
RäDev <github@rayraydev.dev>
2022-07-15 15:01:35 -0600

    More changes.

--- Java Database Connection Practice/Assignment_9/src/Main.java
+++ Java Database Connection Practice/Assignment_9/src/Main.java
@@ -12,10 +12,9 @@ public class Main {
     static final String PASS = "root";
 
 
-    public static void main(String[] args) {
-            try {
-                Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
-                Statement statement = connection.createStatement();
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();
 //                String createDB = "CREATE DATABASE Person";
 //                statement.executeUpdate(createDB);
 //                String createTable = "CREATE TABLE personalInformation" +
@@ -33,35 +32,36 @@ public class Main {
 //                out.println("Insert into table completed successfully!");
 //                out.println("Table successfully created!");
 //                out.println("Database successfully created!");
-                Scanner userInput = new Scanner(System.in);
-                StringBuilder sb1 = new StringBuilder();
-                StringBuilder sb2 = new StringBuilder();
-                int exit = 0;
-                while(exit != -1) {
-                        Person newPerson = new Person();
-                        out.print("Welcome! Type -1 to exit. Please enter the first name of the person you want to enter: ");
-                        newPerson.setFirstName(String.valueOf(sb1.append(String.format("'%s'", userInput.nextLine()))));
-                        out.println(newPerson.getFirstName());
-                        out.print("Now, please enter their last name: ");
-                        newPerson.setLastName(String.valueOf(sb2.append(String.format("'%s'", userInput.nextLine()))));
-                        out.println(newPerson.getLastName());
-                        out.print("Next, enter their age: ");
-                        newPerson.setAge(userInput.nextInt());
-                        out.print("Now, please enter their Social Security Number (do not use anyone's real ssn!): ");
-                        newPerson.setSsn(userInput.nextLong());
-                        out.print("Finally, please enter their primary credit card number (do not enter anyone's real cc number!): ");
-                        newPerson.setCreditCard(userInput.nextLong());
-                        newPerson.insertNewPerson(newPerson);
-                        out.println("Exit? Type -1 to exit, else type any number to insert another record: ");
-                        exit = userInput.nextInt();
-                        if (exit == -1) {
-                            break;
-                        }
+        Scanner userInput = new Scanner(System.in);
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        boolean exit = false;
+        while(!exit) {
+            try {
+                out.print("Welcome! Type -1 to exit. Please enter the person's first name: ");
+                String fn = String.valueOf(sb1.append(String.format("'%s'", userInput.next())));
+                out.println(fn);
+                out.print("Now, please enter their last name: ");
+                String ln = String.valueOf(sb2.append(String.format("'%s'", userInput.next())));
+                out.println(ln);
+                out.print("Next, enter their age: ");
+                int age = userInput.nextInt();
+                out.print("Now, please enter their Social Security Number (do not use anyone's real ssn!): ");
+                long ssn = userInput.nextLong();
+                out.print("Finally, please enter their primary credit card number (do not enter anyone's real cc number!): ");
+                long creditCard = userInput.nextLong();
+                Person newPerson = new Person(fn, ln, age, ssn, creditCard);
+                newPerson.insertPerson(newPerson);
+                out.print("All done? Type -1 to exit, else type any number to insert another record: ");
+                if(userInput.nextInt() == -1) {
+                    exit = true;
+                    if (exit) {
+                        break;
                     }
-
-            } catch (SQLException e) {
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
-
+            }
+    }
 }
--- Java Database Connection Practice/Assignment_9/src/Person.java
+++ Java Database Connection Practice/Assignment_9/src/Person.java
@@ -1,79 +1,42 @@
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InputMismatchException;
 
 import static java.lang.System.out;
 
 public class Person {
-    String firstName;
+    String firstName = null;
+    String lastName = null;
+    int age = 0;
+    long ssn = 0;
+    long creditCard = 0;
 
-    public void setFirstName(String firstName) {
+    Person(String firstName, String lastName, int age, long ssn, long creditCard) {
         this.firstName = firstName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    String lastName;
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    int age;
-
-    public void setAge(int age) {
         this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    long ssn;
-
-    public void setSsn(long ssn) {
         this.ssn = ssn;
-    }
-
-    public long getSsn() {
-        return ssn;
-    }
-
-    long creditCard;
-
-    public void setCreditCard(long creditCard) {
         this.creditCard = creditCard;
     }
 
-    public long getCreditCard() {
-        return creditCard;
-    }
-
-    int i;
-
-
-    Person() {
-    }
+//    int i;
 
     static final String DB_URL = "jdbc:mysql://localhost/Person";
     static final String USER = "root";
     static final String PASS = "root";
 
-    public void insertNewPerson(Person person) {
+    public void insertPerson(Person person) {
         try {
             Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement statement = connection.createStatement();
-            final String insertStatement = "INSERT INTO personalInformation VALUES (" + i + "," + person.getFirstName() + "," + person.getLastName() + "," + person.getAge() + "," + person.getSsn() + "," + person.getCreditCard() + ")";
+            String insertStatement = "INSERT INTO personalInformation (first, last, age, ssn, creditCard) VALUES (" + firstName + "," + lastName + "," + age + "," + ssn + "," + creditCard + ")";
             statement.executeUpdate(insertStatement);
             out.println("Insert into table completed successfully!");
-        } catch (SQLException e) {
+        } catch (SQLException | InputMismatchException e) {
+            out.println("Uh oh! Something went wrong!");
             e.printStackTrace();
         }
     }
